1079a8cdb98e3ae961b781fe36fabeee231fed3f
My Secont Github Repo <hmsadikurrahman120@gmail.com>
2023-02-18 07:50:43 +0600

    complete the triangle part

--- js/index.js
+++ js/index.js
@@ -1,16 +1,40 @@
+let serial = 0;
 
-document.getElementById('triangle-btn').addEventListener('click', function () {
+function getCardTitle(elementId) {
+    return document.getElementById(elementId).innerText;
+}
 
-    const firstInput = document.getElementById('triangle-first-input');
+function getInputValue(inputId) {
+    const firstInput = document.getElementById(inputId);
     const firstInputValueString = firstInput.value;
     const firstInputValue = parseFloat(firstInputValueString);
+    return firstInputValue
+}
+
+function displayData(name, area) {
+
+    const tableContainer = document.getElementById('table-body');
+    serial += 1
+    const tr = document.createElement('tr');
+    tr.innerHTML = `
+    <td>${serial}${'.'}${name}</td>
+    <td>${area}cm<sup>2</sup></td>
+    <td><button
+    class="bg-[#3abff8] px-3 py-2 rounded-md text-white font-medium">Convert to m<sup>2</sup></button></td>
+    `
+    tableContainer.appendChild(tr);
+}
 
 
-    const secondInput = document.getElementById('triangle-second-input');
-    const secondInputValueString = secondInput.value;
-    const secondInputValue = parseFloat(secondInputValueString);
-
-    let totalArea = 0.5 * firstInputValue * secondInputValue;
-    console.log(totalArea)
-
+document.getElementById('triangle-btn').addEventListener('click', function () {
+    //card title
+    const triangleTitle = getCardTitle('triangle-title');
+    //first input value
+    const firstInputValue = getInputValue('triangle-first-input');
+    //second input value
+    const secondInputValue = getInputValue('triangle-second-input')
+    // convert the triangle area
+    let area = 0.5 * firstInputValue * secondInputValue;
+    //show the display data
+    displayData(triangleTitle, area)
 })
